68dc6bb1a348570a1070b99edc2d34b8077e432b
pterrill <peter.terrill@gmail.com>
2011-01-08 20:30:36 -0800

    initial commit for the book's scaffolding example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookDemoApp::Application.config.secret_token = 'e2193237754c1c0baedd8ed28b85c245030e8df306fa30704d52a5e023e3fadf65f4d54ed7aa2a51b10d8a220371bec0f85f9dd86cf76a693035b376a866051d'
